62bf68c7bef644124eb30cd517d66f6adbd2f98c
vonti15 <vonti150802@gmail.com>
2020-04-29 21:51:29 -0600

    Ajustes en la respuesta a formato JSON

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var http = require('http');
 http.createServer(function (req, res) {
+    
         res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-        res.end('Hola mundo Node JS Repo GIT - Ontiveros Aguirre Jesús Vidal\n');
+        var objeto = {
+            mensaje : 'Hola mundo Node JS Repo GIT - Ontiveros Aguirre Jesús Vidal\n'
+        };
+
+        var json = JSON.stringify(objeto);
+
+        res.end(json);
 }).listen(8081);
 console.log('Server running on port 8081');
